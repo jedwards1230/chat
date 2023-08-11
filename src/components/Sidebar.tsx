f372d7198f2e74caafb334fd6d83370086312ddc
Justin <justinedwards1230@gmail.com>
2023-08-11 17:58:29 -0400

    fix: sidebar animation

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -13,13 +13,34 @@ export default function Sidebar({
     className?: string;
     translate: string;
 }) {
-    const ResponsiveWrapper = ({
-        children,
-        className,
-    }: {
-        children: React.ReactNode;
-        className: string;
-    }) => (
+    return (
+        <ResponsiveWrapper
+            close={close}
+            translate={translate}
+            className={clsx(
+                'fixed z-20 h-full w-72 border-r bg-neutral-800 py-2 text-neutral-100 transition-all dark:border-neutral-500 md:flex lg:inset-y-0',
+                className,
+            )}
+        >
+            <div className="relative flex h-full w-full flex-col items-center justify-start gap-4 pb-2 md:pb-0">
+                {children}
+            </div>
+        </ResponsiveWrapper>
+    );
+}
+
+function ResponsiveWrapper({
+    close,
+    children,
+    className,
+    translate,
+}: {
+    close: () => void;
+    children: React.ReactNode;
+    className: string;
+    translate: string;
+}) {
+    return (
         <>
             {/* Desktop */}
             <div className={clsx('hidden md:block', className, translate)}>
@@ -30,6 +51,7 @@ export default function Sidebar({
                 onClick={(e) => {
                     e.stopPropagation();
                     e.preventDefault();
+                    console.log('clicked');
                     close();
                 }}
                 className={clsx(
@@ -42,18 +64,5 @@ export default function Sidebar({
                 </div>
             </div>
         </>
-    );
-
-    return (
-        <ResponsiveWrapper
-            className={clsx(
-                'fixed z-20 h-full w-72 border-r bg-neutral-800 py-2 text-neutral-100 transition-all dark:border-neutral-500 md:flex lg:inset-y-0',
-                className,
-            )}
-        >
-            <div className="relative flex h-full w-full flex-col items-center justify-start gap-4 pb-2 md:pb-0">
-                {children}
-            </div>
-        </ResponsiveWrapper>
     );
 }
