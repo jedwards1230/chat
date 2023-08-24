ded4d7cb067099fde84525335dfd1948e53877a8
Justin <justinedwards1230@gmail.com>
2023-08-24 16:10:46 -0400

    fix: tweak mobile character selector

--- src/components/Dialogs/CharacterSelector.tsx
+++ src/components/Dialogs/CharacterSelector.tsx
@@ -3,10 +3,9 @@
 import { useChat } from '@/providers/ChatProvider';
 import { useUI } from '@/providers/UIProvider';
 import Dialog from './Dialog';
-import { useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import AgentSettings from '../AgentSettings';
 import clsx from 'clsx';
-import { PencilSquare } from '../Icons';
 
 export default function PersonalitySelector() {
     const {
@@ -40,7 +39,7 @@ export default function PersonalitySelector() {
             </div>
             <div className="flex w-full flex-col gap-4 md:flex-row md:gap-2">
                 <select
-                    className="flex-1 rounded px-1 py-2 text-lg md:hidden"
+                    className="hidden flex-1 rounded px-1 py-2 text-lg md:hidden"
                     onChange={(e) => {
                         const agent = characterList.find(
                             (agent) => agent.name === e.target.value,
@@ -75,7 +74,7 @@ export default function PersonalitySelector() {
                         );
                     })}
                 </select>
-                <div className="hidden max-h-[80vh] w-2/5 space-y-2 overflow-y-scroll md:block">
+                <div className="flex max-h-[80vh] w-full gap-2 overflow-y-scroll md:w-2/5 md:flex-col">
                     {characterList.map((agent, i) => {
                         const active =
                             agent.name === activeThread.agentConfig.name;
@@ -110,54 +109,35 @@ function AgentCard({
     edit: () => void;
 }) {
     const { activeThread, updateThreadConfig, setSystemMessage } = useChat();
+    const cardRef = useRef<HTMLDivElement>(null);
 
-    const setActive = () => {
+    const setActive = (e: any) => {
+        e.stopPropagation();
         if (activeThread) {
             updateThreadConfig(agent);
             setSystemMessage(agent.systemMessage);
         }
+        edit();
     };
+
+    useEffect(() => {
+        if (active) {
+            cardRef.current?.scrollIntoView({ behavior: 'smooth' });
+        }
+    }, [active]);
 
     return (
         <div
+            ref={cardRef}
+            onClick={setActive}
             className={clsx(
-                'flex cursor-pointer items-center justify-between rounded-lg shadow transition-colors',
+                'mb-3 flex cursor-pointer items-center whitespace-nowrap rounded-lg px-2 py-1 shadow transition-colors md:mb-0',
                 active
                     ? 'bg-green-400 hover:bg-green-500 dark:bg-green-600 dark:hover:bg-green-700'
                     : 'hover:bg-neutral-200 dark:hover:bg-neutral-600',
             )}
         >
-            <div
-                onClick={setActive}
-                className="h-full w-full px-2 py-1 md:hidden"
-            >
-                <h2 className="text-lg font-medium">{agent.name}</h2>
-                <p>{agent.systemMessage}</p>
-            </div>
-            <div
-                onClick={(e) => {
-                    e.stopPropagation();
-                    setActive();
-                    edit();
-                }}
-                className="hidden h-full w-full flex-col px-2 py-1 md:flex"
-            >
-                {agent.name}
-            </div>
-            <div
-                onClick={(e) => {
-                    e.stopPropagation();
-                    edit();
-                }}
-                className={clsx(
-                    'flex h-8 w-8 flex-shrink-0 items-center justify-center rounded-full transition-colors md:hidden ',
-                    active
-                        ? 'hover:bg-green-600 dark:hover:bg-green-800'
-                        : 'hover:bg-neutral-300 dark:hover:bg-neutral-500',
-                )}
-            >
-                <PencilSquare />
-            </div>
+            {agent.name}
         </div>
     );
 }
