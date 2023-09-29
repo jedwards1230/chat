6bee690cdac3df87b423a57c300c809654e288d0
Justin <justinedwards1230@gmail.com>
2023-09-29 13:38:14 -0400

    feat: error page

---
+++ src/app/global-error.tsx
@@ -0,0 +1,22 @@
+'use client';
+
+export default function GlobalError({
+    error,
+    reset,
+}: {
+    error: Error & { digest?: string };
+    reset: () => void;
+}) {
+    return (
+        <html>
+            <body className="mx-auto flex h-full w-[50vw] flex-col items-center justify-start gap-24 bg-background py-24 text-foreground transition-colors">
+                <h2 className="text-5xl font-medium">Something went wrong!</h2>
+                <p>{error.message}</p>
+                <p>{error.digest}</p>
+                <p className="text-xl">
+                    Go <button onClick={() => reset()}>Try again</button>
+                </p>
+            </body>
+        </html>
+    );
+}
