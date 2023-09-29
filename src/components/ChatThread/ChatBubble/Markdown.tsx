c611e8dc52829dbaaeae1c51f6438d533a44eea6
Justin <justinedwards1230@gmail.com>
2023-09-28 21:47:55 -0400

    chore: bump dependencies

--- src/components/ChatThread/ChatBubble/Markdown.tsx
+++ src/components/ChatThread/ChatBubble/Markdown.tsx
@@ -2,16 +2,16 @@
 
 import clsx from 'clsx';
 import { useMemo, useState } from 'react';
-import { SpecialComponents } from 'react-markdown/lib/ast-to-react';
-import { NormalComponents } from 'react-markdown/lib/complex-types';
 import { PrismAsync as SyntaxHighlighter } from 'react-syntax-highlighter';
 import { vs } from 'react-syntax-highlighter/dist/esm/styles/hljs';
 import { vscDarkPlus } from 'react-syntax-highlighter/dist/esm/styles/prism';
 import { useTheme } from 'next-themes';
-import ReactMarkdown from 'react-markdown';
+import ReactMarkdown, { Components } from 'react-markdown';
 import remarkGfm from 'remark-gfm';
 
 import { Check } from '../../Icons';
+import { Button } from '@/components/ui/button';
+import { Copy } from 'lucide-react';
 
 export default function Markdown({
     content = '',
@@ -23,11 +23,7 @@ export default function Markdown({
     const { resolvedTheme } = useTheme();
     const [btnClicked, setBtnClicked] = useState(false);
 
-    const components = useMemo<
-        Partial<
-            Omit<NormalComponents, keyof SpecialComponents> & SpecialComponents
-        >
-    >(
+    const components = useMemo<Partial<Components>>(
         () => ({
             h1: ({ node, ...props }) => (
                 <h1 {...props} className="text-2xl font-bold" />
@@ -55,12 +51,11 @@ export default function Markdown({
                 />
             ),
             br: ({ node, ...props }) => <br {...props} className="my-1" />,
-            pre: ({ node, ...props }) => <pre {...props} className="" />,
-            code({ node, inline, className, children, ...props }) {
+            code({ node, className, children, ...props }) {
                 const language = className?.replace('language-', '');
                 const match = /language-(\w+)/.exec(className || '');
 
-                const value = (node.children[0] as any).value;
+                const value = (node?.children[0] as any).value;
                 const isImg = value.startsWith('![');
 
                 if (isImg) {
@@ -76,34 +71,18 @@ export default function Markdown({
                     );
                 }
 
-                if (!inline && match) {
+                if (match) {
+                    const Highlight = SyntaxHighlighter as any;
                     return (
-                        <div className="flex flex-col gap-2">
-                            <div className="rounded-t-lg border border-neutral-600 bg-neutral-200 dark:bg-neutral-700">
-                                <div className="p-4 pb-1 pt-2 text-sm">
+                        <div className="rounded-rounded flex flex-col gap-1 border border-neutral-600 bg-neutral-200 p-1 font-normal dark:bg-neutral-700">
+                            <div className="flex items-center justify-between">
+                                <div className="pl-2">
                                     {language ? language : 'code'}
                                 </div>
-                                <SyntaxHighlighter
-                                    {...props}
-                                    style={
-                                        resolvedTheme !== 'dark'
-                                            ? vs
-                                            : vscDarkPlus
-                                    }
-                                    className="!mb-0"
-                                    language={match[1]}
-                                >
-                                    {String(children).replace(/\n$/, '')}
-                                </SyntaxHighlighter>
-                            </div>
-                            <div className="flex items-center justify-center">
-                                <button
-                                    className={clsx(
-                                        btnClicked
-                                            ? 'border-neutral-400 bg-neutral-50 text-green-500 hover:bg-neutral-100 dark:bg-neutral-500 dark:hover:bg-neutral-500'
-                                            : 'border-neutral-500 bg-neutral-50 hover:bg-neutral-200 dark:bg-neutral-300 dark:text-neutral-900',
-                                        'flex justify-center rounded-full border px-3 py-1 text-sm font-medium active:bg-neutral-300 dark:active:bg-neutral-400',
-                                    )}
+                                <Button
+                                    variant="ghost"
+                                    size="icon"
+                                    className="text-foreground"
                                     onClick={() => {
                                         navigator.clipboard.writeText(
                                             String(children),
@@ -114,23 +93,27 @@ export default function Markdown({
                                         }, 1000);
                                     }}
                                 >
-                                    {btnClicked ? <Check /> : 'Copy'}
-                                </button>
+                                    {btnClicked ? <Check /> : <Copy />}
+                                </Button>
                             </div>
+                            <Highlight
+                                {...props}
+                                style={
+                                    resolvedTheme !== 'dark' ? vs : vscDarkPlus
+                                }
+                                className="!m-0"
+                                language={match[1]}
+                            >
+                                {String(children)}
+                            </Highlight>
                         </div>
                     );
                 }
 
                 return (
-                    <div
-                        {...props}
-                        className={clsx(
-                            className,
-                            'inline-block rounded-sm bg-neutral-100 px-0.5 py-0 tracking-wide transition-colors dark:bg-neutral-600',
-                        )}
-                    >
-                        <code className="whitespace-pre-wrap">{children}</code>
-                    </div>
+                    <code className="whitespace-pre-wrap bg-secondary px-0.5 font-bold tracking-widest">
+                        {children}
+                    </code>
                 );
             },
             em: ({ node, ...props }) => <em {...props} className="italic" />,
@@ -138,27 +121,19 @@ export default function Markdown({
                 <strong {...props} className="font-bold" />
             ),
             hr: ({ node, ...props }) => <hr {...props} className="my-2" />,
-            li: ({ node, index, ordered, checked, children, ...props }) => (
-                <li className="flex w-full" {...props}>
-                    <span className="list-marker">
-                        {ordered ? `${index + 1}.` : '-'}
-                    </span>
-                    <div className="">{children}</div>
-                </li>
+            li: ({ node, children, ...props }) => (
+                <li {...props}>{children}</li>
             ),
-            ol: ({ node, depth, ordered, ...props }) => (
+            ol: ({ node, ...props }) => (
                 <ol
                     {...props}
                     className="inline-block list-outside list-decimal"
                 />
             ),
-            ul: ({ node, depth, ordered, ...props }) => (
+            ul: ({ node, ...props }) => (
                 <ul
                     {...props}
-                    className={clsx(
-                        'inline-block list-outside list-disc',
-                        depth > 1 && 'ml-4',
-                    )}
+                    className={clsx('inline-block list-outside list-disc')}
                 />
             ),
             a: ({ node, ...props }) => (
@@ -177,24 +152,21 @@ export default function Markdown({
                 />
             ),
             tbody: ({ node, ...props }) => <tbody {...props} />,
-            td: ({ node, isHeader, ...props }) => (
+            td: ({ node, ...props }) => (
                 <td {...props} className="border border-gray-300 px-2 py-1" />
             ),
-            th: ({ node, isHeader, ...props }) => (
+            th: ({ node, ...props }) => (
                 <th {...props} className="border border-gray-300 px-2 py-1" />
             ),
             thead: ({ node, ...props }) => <thead {...props} />,
-            tr: ({ node, isHeader, ...props }) => (
-                <tr {...props} className="border" />
-            ),
+            tr: ({ node, ...props }) => <tr {...props} className="border" />,
         }),
         [btnClicked, resolvedTheme],
     );
 
     return (
         <ReactMarkdown
-            className="flex w-full flex-col gap-y-4 overflow-x-scroll"
-            linkTarget="_blank"
+            className="flex w-full flex-col gap-y-4 overflow-y-hidden overflow-x-scroll"
             remarkPlugins={[remarkGfm]}
             components={components}
         >
