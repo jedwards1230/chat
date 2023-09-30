74cff305b317174185e1b52dbf5a784386821793
Justin <justinedwards1230@gmail.com>
2023-09-30 15:54:29 -0400

    fix: color contrast

--- src/components/Header/ModelSelector.tsx
+++ src/components/Header/ModelSelector.tsx
@@ -14,14 +14,14 @@ export default function ModelSelector() {
 
     return (
         <div className="flex flex-col items-center justify-center gap-4">
-            <div className="flex rounded-lg bg-neutral-200 p-1 dark:bg-neutral-900">
-                <div className="group relative flex flex-row gap-4">
+            <div className="flex p-1 rounded-lg bg-accent">
+                <div className="relative flex flex-row gap-4 group">
                     <button
                         className={clsx(
-                            'w-32 flex-1 rounded-lg px-4 py-2 text-center font-medium',
+                            'z-10 w-32 flex-1 rounded-lg px-4 py-2 text-center font-medium',
                             isGPT3
                                 ? 'text-neutral-900 dark:text-white'
-                                : 'text-neutral-500 dark:text-neutral-400',
+                                : 'text-neutral-400 dark:text-neutral-400',
                         )}
                         onClick={() =>
                             updateThreadConfig({ model: 'gpt-3.5-turbo-16k' })
@@ -31,10 +31,10 @@ export default function ModelSelector() {
                     </button>
                     <button
                         className={clsx(
-                            'w-32 flex-1 rounded-lg px-4 py-2 text-center font-medium',
+                            'z-10 w-32 flex-1 rounded-lg px-4 py-2 text-center font-medium',
                             isGPT4
                                 ? 'text-neutral-900 dark:text-white'
-                                : 'text-neutral-500 dark:text-neutral-400',
+                                : 'text-neutral-400 dark:text-neutral-400',
                         )}
                         onClick={() => updateThreadConfig({ model: 'gpt-4' })}
                     >
@@ -43,7 +43,7 @@ export default function ModelSelector() {
                     <motion.div
                         layoutId="config-tab"
                         className={clsx(
-                            'absolute top-0 h-full w-32 rounded-lg bg-neutral-400/30 dark:bg-neutral-500/40 sm:group-hover:dark:bg-neutral-600/40',
+                            'absolute top-0 h-full w-32 rounded-lg bg-background sm:group-hover:dark:bg-neutral-600/40',
                             isGPT3 ? 'left-0' : 'right-0',
                         )}
                     />
