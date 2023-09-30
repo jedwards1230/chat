78753a86efdc68d491cff7e5522d0f5d561642e0
Justin <justinedwards1230@gmail.com>
2023-09-30 11:47:30 -0400

    chore: refactor

--- src/components/Header/ModelSelector.tsx
+++ src/components/Header/ModelSelector.tsx
@@ -8,6 +8,10 @@ export default function ModelSelector() {
     const { activeThread, updateThreadConfig } = useChat();
     const activeModel = activeThread.agentConfig.model;
 
+    const isGPT3 =
+        activeModel === 'gpt-3.5-turbo-16k' || activeModel === 'gpt-3.5-turbo';
+    const isGPT4 = activeModel === 'gpt-4' || activeModel === 'gpt-4-0613';
+
     return (
         <div className="flex flex-col items-center justify-center gap-4">
             <div className="flex rounded-lg bg-neutral-200 p-1 dark:bg-neutral-900">
@@ -15,8 +19,7 @@ export default function ModelSelector() {
                     <button
                         className={clsx(
                             'w-32 flex-1 rounded-lg px-4 py-2 text-center font-medium',
-                            activeModel === 'gpt-3.5-turbo-16k' ||
-                                activeModel === 'gpt-3.5-turbo'
+                            isGPT3
                                 ? 'text-neutral-900 dark:text-white'
                                 : 'text-neutral-500 dark:text-neutral-400',
                         )}
@@ -29,8 +32,7 @@ export default function ModelSelector() {
                     <button
                         className={clsx(
                             'w-32 flex-1 rounded-lg px-4 py-2 text-center font-medium',
-                            activeModel === 'gpt-4' ||
-                                activeModel === 'gpt-4-0613'
+                            isGPT4
                                 ? 'text-neutral-900 dark:text-white'
                                 : 'text-neutral-500 dark:text-neutral-400',
                         )}
@@ -42,10 +44,7 @@ export default function ModelSelector() {
                         layoutId="config-tab"
                         className={clsx(
                             'absolute top-0 h-full w-32 rounded-lg bg-neutral-400/30 dark:bg-neutral-500/40 sm:group-hover:dark:bg-neutral-600/40',
-                            activeModel === 'gpt-3.5-turbo-16k' ||
-                                activeModel === 'gpt-3.5-turbo'
-                                ? 'left-0'
-                                : 'right-0',
+                            isGPT3 ? 'left-0' : 'right-0',
                         )}
                     />
                 </div>
