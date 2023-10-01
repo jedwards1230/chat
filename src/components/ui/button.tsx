7ea977017ab6e95e1cc335d9abb562c0c2849c27
Justin Edwards <justinedwards1230@gmail.com>
2023-10-01 12:57:13 -0400

    fix: colors

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -14,7 +14,7 @@ const buttonVariants = cva(
                 destructive:
                     'bg-destructive text-destructive-foreground hover:bg-destructive/90',
                 outline:
-                    'border border-input bg-accent hover:bg-background/20 hover:text-accent-foreground',
+                    'border border-input text-accent-foreground bg-accent hover:bg-background/50 hover:dark:bg-background/20 hover:text-foreground',
                 secondary:
                     'bg-secondary text-secondary-foreground hover:bg-secondary/80',
                 ghost: 'hover:bg-accent hover:text-accent-foreground',
