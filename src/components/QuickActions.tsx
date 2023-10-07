c53426728da3222c261fc664116b238a3cad2c7e
Justin Edwards <justinedwards1230@gmail.com>
2023-10-07 12:23:44 -0400

    chore: cleanup quick actions

--- src/components/QuickActions.tsx
+++ src/components/QuickActions.tsx
@@ -1,13 +1,12 @@
 'use client';
 
-import clsx from 'clsx';
-import Link from 'next/link';
-
 import { useChat } from '@/providers/ChatProvider';
 import useMessages from '@/lib/ChatManagerHook';
+import { Button } from './ui/button';
 
 export default function QuickActions() {
-    const { currentThread, threads, botTyping, abortRequest } = useChat();
+    const { currentThread, threads, botTyping, createThread, abortRequest } =
+        useChat();
     const activeThread =
         currentThread !== null ? threads[currentThread] : undefined;
     const messages = useMessages(
@@ -15,32 +14,18 @@ export default function QuickActions() {
         activeThread?.mapping,
     );
 
-    const btn = 'px-3 py-1.5 rounded';
-
     if (!activeThread) return null;
     return (
-        <div className="absolute inset-x-0 flex justify-end gap-2 px-5 text-sm font-medium -top-10 text-background dark:text-foreground">
+        <div className="absolute inset-x-0 -top-10 flex justify-end gap-2 px-5 text-sm font-medium text-background dark:text-foreground">
             {botTyping && (
-                <button
-                    onClick={abortRequest}
-                    className={clsx(btn, 'bg-red-500')}
-                >
+                <Button variant="destructive" size="xs" onClick={abortRequest}>
                     Stop
-                </button>
+                </Button>
             )}
             {messages.length > 1 && (
-                <>
-                    <Link replace={true} href="/">
-                        <button
-                            className={clsx(
-                                btn,
-                                'bg-blue-500 hover:bg-blue-400',
-                            )}
-                        >
-                            New Chat
-                        </button>
-                    </Link>
-                </>
+                <Button variant="primaryBlue" size="xs" onClick={createThread}>
+                    New Chat
+                </Button>
             )}
         </div>
     );
