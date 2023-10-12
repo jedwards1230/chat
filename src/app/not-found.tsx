db1aefa8f7c7e4afc111ccd9947705b066095d04
Justin Edwards <justinedwards1230@gmail.com>
2023-10-12 13:22:10 -0400

    fix: not found page

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,14 +1,32 @@
-import Link from 'next/link';
+'use client';
+
+import { useRouter } from 'next/navigation';
+
+import { Button } from '@/components/ui/button';
 
 export default function NotFound() {
+    const router = useRouter();
     return (
-        <div className="flex h-full w-full flex-col items-center justify-start gap-48 py-24">
+        <div className="flex h-full w-full flex-col items-center justify-start gap-24 py-24">
             <h2 className="text-6xl font-medium">Not Found</h2>
-            <p className="text-xl">
-                Go{' '}
-                <Link className="text-blue-500" href="/">
+            <p className="text-lg">
+                <Button
+                    className="text-lg"
+                    variant="link"
+                    size="xs"
+                    onClick={() => router.replace('/')}
+                >
                     Home
-                </Link>
+                </Button>{' '}
+                |{' '}
+                <Button
+                    className="text-lg"
+                    variant="link"
+                    size="xs"
+                    onClick={() => router.refresh()}
+                >
+                    Refresh
+                </Button>
             </p>
         </div>
     );
